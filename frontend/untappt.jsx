eda82d49f7b24a20e3df6df5e37e79b871600038
App Academy Student <aa@Apps-Mac-mini-98.local>
2019-10-01 18:34:05 -0400

    Test 2

--- frontend/untappt.jsx
+++ frontend/untappt.jsx
@@ -1,6 +1,12 @@
-// //React
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// //Components
-// import Root from './components/root';
-// import configureStore from './store/store';
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Components
+import Root from './components/root';
+import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let root = document.getElementById('root');
+  let store = configureStore();
+  ReactDOM.render(<Root store={store}/>, root);
+})
