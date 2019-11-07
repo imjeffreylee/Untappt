1df52d2e12faf11676e53ea55b927e69163f4701
imjeffreylee <imjeffreylee@gmail.com>
2019-11-06 20:30:09 -0500

    Added fields and styles for user sign up page

--- frontend/components/session_forms/session_form.jsx
+++ frontend/components/session_forms/session_form.jsx
@@ -42,9 +42,9 @@ class SessionForm extends React.Component {
   }
 
   renderErrors() {
-    if (this.props.errors) {
+    if (this.props.errors.length !== 0) {
       return (
-        <ul>
+        <ul className="err-ul">
           {this.props.errors.map((err, i) => {
             return (
               <li key={i} className="err-msg">
@@ -94,7 +94,7 @@ class SessionForm extends React.Component {
                 <button>{this.props.formType}</button>
               </div>
               <p className="login-signup-switch">
-                Not with us yet? <Link to="/signUp" onClick={this.props.clearErrors}>Sign up</Link>
+                New around here? <Link to="/signUp" onClick={this.props.clearErrors}>Sign up!</Link>
               </p>
               <p className="guest-user">
                 or <a onClick={this.handleGuest}>try it as a guest</a>
@@ -106,33 +106,64 @@ class SessionForm extends React.Component {
     } else if (this.props.formType === "Sign up") {
       return (
         <div className="session-form-container">
-          <form onSubmit={this.handleSubmit} className="login-signup">
-            <div className="inner-session-form-container">
+          <form onSubmit={this.handleSubmit} className="signup">
+            <div className="inner-signup-form-container">
               <div className="form-header josefin">
                 <Link to="/">
                   <h2>UNTAPPT</h2>
                   <h6>DRINK SOCIALLY</h6>
                 </Link>
               </div>
+              <p className="brewery-warning">
+                Are you a brewery that's trying to get added to Untappt, claim and manage your brewery page, or access your brewery account? Check this out first:
+                <a href="#">
+                  &nbsp;Getting on Untappt: Brewery Guide
+                </a>
+              </p>
+              <p className="all-fields">
+                All fields below are required unless specified.
+              </p>
               {this.renderErrors()}
               <div className="input-holder">
-                <div className="icon-input-wrapper">
-                  <i className="fas fa-user"></i>
-                  <input type="text"
-                    value={this.state.username}
-                    onChange={this.update('username')}
-                    placeholder="Username"
-                    className="input-username"
-                  />
+                <div className="user-email">
+                  <div className="icon-input-wrapper">
+                    <i className="fas fa-user"></i>
+                    <input type="text"
+                      value={this.state.username}
+                      onChange={this.update('username')}
+                      placeholder="Username"
+                      className="input-username"
+                    />
+                  </div>
+                  <div className="icon-input-wrapper">
+                    <i className="fas fa-envelope"></i>
+                    <input type="email"
+                      value={this.state.email}
+                      onChange={this.update('email')}
+                      placeholder="Email Address"
+                      className="input-email"
+                    />
+                  </div>
                 </div>
-                <div className="icon-input-wrapper">
-                  <i className="fas fa-unlock"></i>
-                  <input type="password"
-                    value={this.state.password}
-                    onChange={this.update('password')}
-                    placeholder="Password"
-                    className="input-password"
-                  />
+                <div className="password-confirm-password">
+                  <div className="icon-input-wrapper">
+                    <i className="fas fa-unlock"></i>
+                    <input type="password"
+                      value={this.state.password}
+                      onChange={this.update('password')}
+                      placeholder="Password"
+                      className="input-password"
+                    />
+                  </div>
+                  <div className="icon-input-wrapper">
+                    <i className="fas fa-unlock"></i>
+                    <input type="password"
+                      value={this.state.password}
+                      onChange={this.update('password')}
+                      placeholder="Repeat password"
+                      className="input-password"
+                    />
+                  </div>
                 </div>
               </div>
               <div className="button-holder">
