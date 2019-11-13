a753dbf3ebac1a56dea8c43aaa0390a6d6943601
imjeffreylee <imjeffreylee@gmail.com>
2019-11-13 17:04:48 -0500

    Added validation to checkin model to limit rating from 0 to 5

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -2,6 +2,7 @@ class Checkin < ApplicationRecord
   validates :user_id, :drink_id, :rating, presence: true
   validates :rating, inclusion: { 0..5 }
 
+  
   belongs_to :user
   belongs_to :drink
 end
