c161ad742b071ea07363b9313bd151e0ef603c24
imjeffreylee <imjeffreylee@gmail.com>
2019-11-17 17:00:15 -0500

    Created modal container, modal jsx, and ui reducer

---
+++ frontend/components/modals/modal.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { closeModal } from "../../actions/modal_actions";
+import { connect } from "react-redux";
+import CheckinFormContainer from "../checkins/checkin_form_container";
+
+const Modal = (props) => {
+    if (!props.modal) {
+        return null;
+    }
+
+    return (
+        <div className="modal-background" onClick={props.closeModal}>
+            <div className="modal-child" onClick={e => e.stopPropagation()}>
+                <CheckinFormContainer drink={props.drink} />
+            </div>
+        </div>
+    )
+}
+
+const msp = state => {
+    modal: state.ui.modal
+};
+
+const mdp = dispatch => {
+    closeModal: () => dispatch(closeModal())
+};
+
+export default connect(msp, mdp)(Modal);
