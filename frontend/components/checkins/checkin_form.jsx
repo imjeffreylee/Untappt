00900d3c573d936166fcaa91776cb2648cb466de
imjeffreylee <imjeffreylee@gmail.com>
2019-11-17 20:35:18 -0500

    Added checkin form

---
+++ frontend/components/checkins/checkin_form.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class CheckinForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            drink_id: this.props.drink.id,
+            review: "",
+            rating: 5
+        }
+
+    }
+
+    // handleChange(field) {
+    //     return(e) {
+
+    //     }
+    // }
+}
