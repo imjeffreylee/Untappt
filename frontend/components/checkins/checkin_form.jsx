5ac8b02b71362b52a51faf728ee34c62a9a4811b
imjeffreylee <imjeffreylee@gmail.com>
2019-11-21 16:19:41 -0500

    Added more function to checkin

--- frontend/components/checkins/checkin_form.jsx
+++ frontend/components/checkins/checkin_form.jsx
@@ -6,14 +6,28 @@ class CheckinForm extends React.Component {
         this.state = {
             drink_id: this.props.drink.id,
             review: "",
-            rating: 5
+            rating: 5,
+            slideVal: 8
         }
-
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    // handleChange(field) {
-    //     return(e) {
+    handleChange(field) {
+        return (e) => {
+            this.setState({ [field]: e.target.value }, () => {
+                this.setState({ 'rating': (this.state.slideVal * 0.25) })
+            });
+        };
+    }
 
-    //     }
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createCheckin(this.state.drink_id, this.state).then(() => this.props.closeModal());
+    }
+
+    // render() {
+    //     return (
+
+    //     )
     // }
 }
