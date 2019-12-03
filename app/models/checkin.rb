56cb6422d65b607d7eb5034090ff7bcd7a5d06bf
imjeffreylee <imjeffreylee@gmail.com>
2019-12-02 22:57:15 -0500

    Added association to checkin model for breweries, added more seeds

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -2,5 +2,10 @@ class Checkin < ApplicationRecord
   validates :user_id, :drink_id, :rating, presence: true
 
   belongs_to :user
+
   belongs_to :drink
+  
+  has_one :brewery,
+    through: :drink,
+    source: :brewery
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 User.destroy_all
 Brewery.destroy_all
 Drink.destroy_all
+Checkin.destroy_all
 
 #### Users seeds
 
@@ -41,42 +42,42 @@ guest2 = User.create(
 
 #### Breweries seeds
 
-BBC_B = Brewery.create(
+brew1 = Brewery.create(
   brewery_name: "Boston Beer Company",
   brewery_type: "Regional Brewery",
   origin: "Boston, MA United States",
   brewery_description: "Jim Koch founded the Boston Beer Company back in 1984 after brewing the first batch of Samuel Adams Boston Lager in his kitchen."
 )
 
-STA_B = Brewery.create(
+brew2 = Brewery.create(
   brewery_name: "Stella Artois",
   brewery_type: "Macro Brewery",
   origin: "Leuven, Vlaanderen Belgium",
   brewery_description: "Stella Artois was first brewed as a Christmas beer in Leuven."
 )
 
-HB_B = Brewery.create(
+brew3 = Brewery.create(
   brewery_name: "Harpoon Brewery",
   brewery_type: "Regional Brewery",
   origin: "Boston, MA United States",
   brewery_description: "Love Beer. Love Life. Employee-Owned, brewed in Boston, MA and Windsor, VT."
 )
 
-KBB_B = Brewery.create(
+brew4 = Brewery.create(
   brewery_name: "Kronenbourg Brewery",
   brewery_type: "Macro Brewery",
   origin: "Strasbourg, Grand-Est France",
   brewery_description: "Kronenbourg 1664 is the most sold French beer in the world and the market leader for high-end premium beers."
 )
 
-KBC_B = Brewery.create(
+brew5 = Brewery.create(
   brewery_name: "Kirin Brewery Company",
   brewery_type: "Macro Brewery",
   origin: "Nakano-ku, Tokyo Japan",
   brewery_description: "Japanese brewery."
 )
 
-TTCL_B = Brewery.create(
+brew6 = Brewery.create(
   brewery_name: "Taiwan Tobacco & Liquor Corporation",
   brewery_type: "Macro Brewery",
   origin: "Taiwan",
@@ -89,54 +90,54 @@ TTCL_B = Brewery.create(
 
 #### Drinks seeds
 
-OKT_D = Drink.create(
+drink1 = Drink.create(
   drink_name: "Samuel Adams OctoberFest",
-  brewery_id: BBC_B.id,
+  brewery_id: brew1.id,
   style: "Märzen",
   ABV: 5.3,
   IBU: 16,
   description: "In 1810, the Oktoberfest tradition was born when Munich celebrated the Crown Prince’s wedding with a special beer and 16 day party."
 )
 
-STA_D = Drink.create(
+drink2 = Drink.create(
   drink_name: "Stella Artois",
-  brewery_id: STA_B.id,
+  brewery_id: brew2.id,
   style: "Lager - Euro Pale",
   ABV: 5,
   IBU: 24,
   description: "Stella Artois was first brewed as a Christmas beer in Leuven."
 )
 
-HIPA_D = Drink.create(
+drink3 = Drink.create(
   drink_name: "Harpoon IPA",
-  brewery_id: HB_B.id,
+  brewery_id: brew3.id,
   style: "IPA - American",
   ABV: 5.9,
   IBU: 42,
   description: "We started brewing this beer as a summer seasonal in 1993."
 )
 
-one_six_six_four_D = Drink.create(
+drink4 = Drink.create(
   drink_name: "1664",
-  brewery_id: KBB_B.id,
+  brewery_id: brew4.id,
   style: "Lager - Euro Pale",
   ABV: 5.5,
   IBU: 20,
   description: "Kronenbourg 1664 is France’s No.1 selling beer and the UK’s second-biggest selling premium lager."
 )
 
-ICHI_D = Drink.create(
+drink5 = Drink.create(
   drink_name: "Ichiban (Ichiban Shibori) (一番搾り)",
-  brewery_id: KBC_B.id,
+  brewery_id: brew5.id,
   style: "Lager - Pale",
   ABV: 5,
   IBU: 18,
   description: "Kirin Ichiban Shibori is the name in the Japanese Market, whilst Kirin Ichiban is the name for export."
 )
 
-GMTB_D = Drink.create(
+drink6 = Drink.create(
   drink_name: "Gold Medal Taiwan Beer",
-  brewery_id: TTCL_B.id,
+  brewery_id: brew6.id,
   style: "Lager - Japanese Rice",
   ABV: 5,
   IBU: 0,
@@ -149,11 +150,46 @@ GMTB_D = Drink.create(
 
 #### Checkins seeds
 
-Checkin1 = Checkin.create(
+checkin1 = Checkin.create(
   user_id: guest.id,
-  drink_id: one_six_six_four_D.id,
+  drink_id: drink1.id,
+  rating: 5,
+  review: "This is so good!"
+)
+  
+checkin2 = Checkin.create(
+  user_id: guest.id,
+  drink_id: drink2.id,
+  rating: 5,
+  review: "I can drink this all day!"
+)
+    
+checkin3 = Checkin.create(
+  user_id: guest.id,
+  drink_id: drink3.id,
+  rating: 3,
+  review: "It was okay."
+)
+      
+checkin4 = Checkin.create(
+  user_id: guest.id,
+  drink_id: drink4.id,
   rating: 1,
   review: "I'm not sure I'm drinking water or beer."
+)
+
+checkin5 = Checkin.create(
+  user_id: guest.id,
+  drink_id: drink5.id,
+  rating: 3,
+  review: "It was okay."
+)
+
+checkin6 = Checkin.create(
+  user_id: guest.id,
+  drink_id: drink6.id,
+  rating: 2,
+  review: "I drink it only when there are no other choices."
 )
 
 #### Checkins seeds end
