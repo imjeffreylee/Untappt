ec3bbd560394bc73c06aed1a80d14220a775b285
imjeffreylee <imjeffreylee@gmail.com>
2019-12-10 23:01:40 -0500

    Created modal for new checkins

--- frontend/actions/modal_actions.js
+++ frontend/actions/modal_actions.js
@@ -1,10 +1,13 @@
 export const LAUNCH_MODAL = "LAUNCH_MODAL";
 export const CLOSE_MODAL = "CLOSE_MODAL";
 
-export const launchModal = (modal) => ({
-    type: LAUNCH_MODAL,
-    modal
-})
+export const launchModal = (modal) => {
+    // debugger
+    return ({
+        type: LAUNCH_MODAL,
+        modal
+    })
+}
 
 export const closeModal = () => ({
     type: CLOSE_MODAL
